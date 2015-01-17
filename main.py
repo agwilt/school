ff569a05d1471c1d2e2da4fab7ef8a16b5199844
Andreas Gwilt <andreas@gwilt.net>
2015-01-17 23:18:31 +0100

    got it *working*

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import time, copy
 
-world = [[0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0],
-		 [0,0,0,0,0,0,0,0,0,0]]
+wl = 50
 
-wl = len(world)
+hl = 100
+vl = 50
+
+world = [[0 for i in range(wl)] for j in range(wl)]
+
+world[10][10] = 1
+world[10][11] = 1
+world[10][12] = 1
+world[8][11] = 1
+world[9][12] = 1
 
 def update(oldworld):
 	newworld = [[0 for i in range(wl)] for j in range(wl)]
 	for row in range(wl):
-		print("***ROW %d***" % row)
 		for col in range(wl):
 			ncount = 0
-			print("COL %d" % col)
-			ncount = oldworld[row][(col-1)%wl] + oldworld[row][(col+1)%wl]
 			ncount += oldworld[(row-1)%wl][(col-1)%wl] + oldworld[(row-1)%wl][col] + oldworld[(row-1)%wl][(col+1)%wl]
+			ncount += oldworld[row][(col-1)%wl] + oldworld[row][(col+1)%wl]
 			ncount += oldworld[(row+1)%wl][(col-1)%wl] + oldworld[(row+1)%wl][col] + oldworld[(row+1)%wl][(col+1)%wl]
-			print(ncount)
 			if (ncount < 2) or (ncount > 3):
 				newworld[row][col] = 0
 			elif ncount == 2 and oldworld[row][col] == 1:
@@ -44,5 +41,5 @@ def draw(world):
 
 while True:
 	draw(world)
-	time.sleep(0.5)
+	time.sleep(0.1)
 	world = update(world)

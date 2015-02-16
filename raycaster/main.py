24f1ad7bf7f450dec238f8b7410cdc90aba3bff9
Andreas Gwilt <andreas@gwilt.net>
2015-02-16 20:50:37 +0100

    Add TODO comment to code. Heh.

--- raycaster/main.py
+++ raycaster/main.py
@@ -163,7 +163,7 @@ def cast(world, p_x, p_y, a):
 			h_x = p_x
 		else:
 			x_i = int(-1 * y_i / math.tan(a))
-			h_x = p_x # more magic. can't think now. do later
+			h_x = p_x # TODO: write this bit
 
 		for i in range(vl):
 			try:

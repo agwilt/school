9b3e6aa15e5049e8d2455a1563a85e8db5a8e067
Andreas Gwilt <andreas@gwilt.net>
2015-02-21 00:41:20 +0100

    Change constant names to be all-upper-case

--- raycaster/main.py
+++ raycaster/main.py
@@ -7,8 +7,8 @@ import pygame
 from pygame.locals import *
 
 # Angles are measured in radians, because that's what the math module uses.
-# Thus, right is 0, down is math.pi, left is math.pi, and up is 1.5*math.pi.
-# Also, an angle should always be positive, and under 2*math.pi.
+# Thus, right is 0, down is PI, left is PI, and up is 1.5*PI.
+# Also, an angle should always be positive, and under 2*PI.
 
 #   y
 #   |
@@ -21,38 +21,38 @@ from pygame.locals import *
 # 0 +---------------x
 #   0  1  2  3  4
 
-debug = False
-god = False
+DEBUG = False
+GOD = False
 
 if "-d" in sys.argv:
-	debug = True
-	god = True
+	DEBUG = True
+	GOD = True
 
 if "-g" in sys.argv:
-	god = True
+	GOD = True
 
 # Constants (not really meant to be configurable)
 TILE = 32
-pi = math.pi
+PI = math.pi
 
 # Variables (maybe read from config file?)
-plane_x = 640  # resolution of plane/screen
-plane_y = 480
-fov = math.radians(60)
-step = 20
-turn = math.radians(5)
+PLANE_X = 640  # resolution of plane/screen
+PLANE_Y = 480
+FOV = math.radians(60)
+STEP = 20
+TURN = math.radians(5)
 
 # World Variables
-hl = 100  # horiz, vert height of field
-vl = 50
-world = [[0 for i in range(vl)] for j in range(hl)]
+HL = 100  # horiz, vert height of field
+VL = 50
+world = [[0 for i in range(VL)] for j in range(HL)]
 p_x = 32  # player x,y
 p_y = 32
 p_a = 0  # pointing right
 
 # computed variables
-plane_d = (plane_x / 2) / math.tan(fov/2)  #distance from player to plane
-ray_angle = fov / plane_x  # angle between rays
+PLANE_D = (PLANE_X / 2) / math.tan(FOV/2)  #distance from player to plane
+RAY_ANGLE = FOV / PLANE_X  # angle between rays
 
 world[8][13] = 1
 world[9][14] = 1
@@ -64,15 +64,15 @@ world[10][14] = 1
 def update(oldworld):
 	"""Run one *life* iteration, return the new world"""
 	# Initialize a completely new array, to clear up confusion.
-	newworld = [[0 for i in range(vl)] for j in range(hl)]
+	newworld = [[0 for i in range(VL)] for j in range(HL)]
 
 	# This loops through *every* cell in the world.
-	for col in range(hl):
-		for row in range(vl):
+	for col in range(HL):
+		for row in range(VL):
 			# Check row above, middle row, and row beneath.
-			ncount = oldworld[(col-1) % hl][(row-1) % vl] + oldworld[col][(row-1) % vl] + oldworld[(col+1) % hl][(row-1) % vl]
-			ncount += oldworld[(col-1) % hl][row] + oldworld[(col+1) % hl][row]
-			ncount += oldworld[(col-1) % hl][(row+1) % vl] + oldworld[col][(row+1) % vl] + oldworld[(col+1) % hl][(row+1) % vl]
+			ncount = oldworld[(col-1) % HL][(row-1) % VL] + oldworld[col][(row-1) % VL] + oldworld[(col+1) % HL][(row-1) % VL]
+			ncount += oldworld[(col-1) % HL][row] + oldworld[(col+1) % HL][row]
+			ncount += oldworld[(col-1) % HL][(row+1) % VL] + oldworld[col][(row+1) % VL] + oldworld[(col+1) % HL][(row+1) % VL]
 			# Do things depending on the number of neighbouring cells.
 			if (ncount < 2) or (ncount > 3):
 				newworld[col][row] = 0
@@ -96,7 +96,7 @@ def cast(world, p_x, p_y, a):
 	if type(p_x) != int:
 		exit(123)
 
-	# max. it: use hl, vl
+	# max. it: use HL, VL
 	# {x,y}_i are intervals, h_{x,y} is the horiz point, v_{x,y} vertical
 
 	# If we are standing *in* a block, we return 0, which dist_to_offset() can handle.
@@ -110,11 +110,11 @@ def cast(world, p_x, p_y, a):
 	# First we are casting for vertical intersections.
 	# If the angle is pointing up or down, don't bother checking.
 	# If the angle is left or right, we check with y_i = 0 and x_i = (-)TILE.
-	if not (a == 0.5*pi or a == 1.5*pi):
+	if not (a == 0.5*PI or a == 1.5*PI):
 
 		# Get x_i (+TILE if pointing right, -TILE if left).
 		# Get v_x (x coord of the first point).
-		if (a < 0.5 * pi) or (a > 1.5 * pi):  # pointing right
+		if (a < 0.5 * PI) or (a > 1.5 * PI):  # pointing right
 			x_i = TILE
 			v_x = (p_x // TILE)*TILE + TILE
 		else:
@@ -127,7 +127,7 @@ def cast(world, p_x, p_y, a):
 		v_y = int(p_y - math.tan(a)*(v_x-p_x))
 
 		while True:
-			if v_x < 0 or v_y < 0 or v_x >= (hl*TILE) or v_y >= (vl*TILE):
+			if v_x < 0 or v_y < 0 or v_x >= (HL*TILE) or v_y >= (VL*TILE):
 				break
 			if world[v_x // TILE][v_y // TILE]:
 				vvalid = True
@@ -140,10 +140,10 @@ def cast(world, p_x, p_y, a):
 	
 	# OK, now we'll check horizontal intersections.
 	# Again, we won't check if the ray is 0° or 180°.
-	if not (a == 0 or a == pi):
+	if not (a == 0 or a == PI):
 
 		# Get y_i and h_y
-		if a < pi:  # pointing down
+		if a < PI:  # pointing down
 			y_i = -1 * TILE
 			h_y = (p_y // TILE)*TILE - 1
 		else:
@@ -151,7 +151,7 @@ def cast(world, p_x, p_y, a):
 			h_y = (p_y // TILE)*TILE + TILE
 
 		# Get x_i and h_x
-		if a == 0.5*pi or a == 1.5*pi:  # vertical ray
+		if a == 0.5*PI or a == 1.5*PI:  # vertical ray
 			x_i = 0
 			h_x = p_x
 		else:
@@ -159,7 +159,7 @@ def cast(world, p_x, p_y, a):
 			h_x = int(p_x - ((p_y - h_y) / math.tan(a)))
 
 		while True:
-			if h_x < 0 or h_y < 0 or h_x >= (hl*TILE) or h_y >= (vl*TILE):
+			if h_x < 0 or h_y < 0 or h_x >= (HL*TILE) or h_y >= (VL*TILE):
 				break
 			if world[h_x // TILE][h_y // TILE]:
 				hvalid = True
@@ -183,12 +183,12 @@ def cast(world, p_x, p_y, a):
 def dist_to_offset(dist):
 	"""Take a distance (to an object), and return the offset from the middle to start drawing the column."""
 	if dist == 0:
-		return plane_y * 0.5
+		return PLANE_Y * 0.5
 	# Distance of -1 means no collision. So it shouldn't render anything.
 	elif dist == -1:
 		return 0
 	else:
-		return (TILE / dist) * plane_d * 0.5
+		return (TILE / dist) * PLANE_D * 0.5
 
 
 def walk(world, p_x, p_y, a):
@@ -196,9 +196,9 @@ def walk(world, p_x, p_y, a):
 	# If we're in a block, don't walk.
 	if world[p_x // TILE][p_y // TILE] == 1:
 		return (p_x,p_y)
-	if cast(world, p_x, p_y, p_a) >= step:  # TODO: Make this better.
-		p_y = int(p_y - (math.sin(a) * step)) % (vl*TILE)
-		p_x = int(p_x + (math.cos(a) * step)) % (hl*TILE)
+	if cast(world, p_x, p_y, p_a) >= STEP:  # TODO: Make this better.
+		p_y = int(p_y - (math.sin(a) * STEP)) % (VL*TILE)
+		p_x = int(p_x + (math.cos(a) * STEP)) % (HL*TILE)
 	return (p_x, p_y)
 
 
@@ -206,26 +206,26 @@ def draw(world):
 	"""render the scene, by casting rays for each column"""
 	# First we draw the background.
 	screen.fill((255,255,255))
-	pygame.draw.rect(screen, (200,200,200),	((0,(plane_y/2)),(plane_x,plane_y)))
+	pygame.draw.rect(screen, (200,200,200),	((0,(PLANE_Y/2)),(PLANE_X,PLANE_Y)))
 
 	# Now we get the angle of the first ray.
-	angle = (p_a - (fov/2)) % (2*pi)
+	angle = (p_a - (FOV/2)) % (2*PI)
 
 	# For all columns, cast a ray, and draw a vertical line on the screen.
-	for col in range(plane_x):
+	for col in range(PLANE_X):
 		dist = cast(world, p_x, p_y, angle)
 		if dist != -1:
-			pygame.draw.line(screen, (0,0,0), (col,((plane_y/2) - dist_to_offset(dist))), (col, (plane_y/2) + dist_to_offset(dist)))
-		angle = (angle + ray_angle) % (2*pi)
+			pygame.draw.line(screen, (0,0,0), (col,((PLANE_Y/2) - dist_to_offset(dist))), (col, (PLANE_Y/2) + dist_to_offset(dist)))
+		angle = (angle + RAY_ANGLE) % (2*PI)
 	pygame.display.flip()
 
-	# If the debug flag is set, we print debug information.
-	if debug:
+	# If the DEBUG flag is set, we print DEBUG information.
+	if DEBUG:
 		print("(%d,%d) %d" % (p_x,p_y,math.degrees(p_a)))
 	# If the map flag is set, print a map to stdout.
 	if "-m" in sys.argv:
-		for row in range(vl-1,-1,-1):
-			for col in range(hl):
+		for row in range(VL-1,-1,-1):
+			for col in range(HL):
 				if (row,col) == (p_x // TILE,p_y // TILE):
 					print('x',end='')
 				elif world[col][row] == 0:
@@ -238,7 +238,7 @@ def draw(world):
 
 # Initialize clock and pygame stuff.
 pygame.init()
-screen = pygame.display.set_mode((plane_x,plane_y))
+screen = pygame.display.set_mode((PLANE_X,PLANE_Y))
 clock = pygame.time.Clock()
 tick = 30
 paused = False
@@ -263,18 +263,18 @@ while True:
 		if keys[K_UP]:  # go forwards
 			p_x, p_y = walk(world, p_x, p_y, p_a)
 		if keys[K_DOWN]:  # go back
-			p_x, p_y = walk(world, p_x, p_y, (p_a + pi))
+			p_x, p_y = walk(world, p_x, p_y, (p_a + PI))
 		if keys[K_COMMA]:  # strafe left
-			p_x, p_y = walk(world, p_x, p_y, (p_a - (0.5*pi)))
+			p_x, p_y = walk(world, p_x, p_y, (p_a - (0.5*PI)))
 		if keys[K_PERIOD]:  # strafe right
-			p_x, p_y = walk(world, p_x, p_y, (p_a + (0.5*pi)))
+			p_x, p_y = walk(world, p_x, p_y, (p_a + (0.5*PI)))
 		if keys[K_LEFT]:  # turn left
-			p_a = (p_a - turn) % (2*pi)
+			p_a = (p_a - TURN) % (2*PI)
 		if keys[K_RIGHT]:  # turn right
-			p_a = (p_a + turn) % (2*pi)
+			p_a = (p_a + TURN) % (2*PI)
 
-	# Check if you died. We don't want this in debug mode.
-	if not god:
+	# Check if you died. We don't want this in DEBUG mode.
+	if not GOD:
 		if world[p_x // TILE][p_y // TILE] == 1:
 			print("You got mown over. By a *cell*.")
 			quit()

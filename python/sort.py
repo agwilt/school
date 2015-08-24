20b664f2f4a664b8b2c091d40bc189f9c35f5d06
Andreas Gwilt <andreas@gwilt.net>
2015-08-24 15:05:51 +0200

    Can't remember, ages ago

--- python/sort.py
+++ python/sort.py
@@ -2,6 +2,12 @@
 
 import sys
 
+def is_sorted(array):
+	for x in range(len(array)-1):
+		if array[x] > array[x+1]:
+			return False
+	return True
+
 def sort(array):
 
 	unsorted = array[:]
@@ -20,5 +26,37 @@ def sort(array):
 
 	return sorted_array
 
+
+def bubblesort(old_array):
+
+	array = old_array[:]
+
+	while not is_sorted(array):
+
+		for x in range(len(array)-1):
+			if array[x] > array[x+1]:
+				array[x], array[x+1] = array[x+1], array[x]
+	return array
+
+
+def asort(array):
+
+	if len(array) == 1:
+		return array
+	elif len(array) == 0:
+		return []
+
+	middle = (max(array) + min(array)) // 2
+	lower = []
+	higher = []
+	for element in array:
+		if element > middle:
+			higher.append(element)
+		else:
+			lower.append(element)
+	return asort(lower) + asort(higher)
+
+
+
 if __name__ == "__main__":
-	print(sort([ int(x) for x in sys.argv[1:]]))
+	print(asort([ int(x) for x in sys.argv[1:]]))
